cd5387516e8a985b25f650a6c9bc5415c73df13d
Jack <jhkent88@gmail.com>
2018-08-14 12:51:08 -0700

    Create initial js and css files

---
+++ main.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyAbxy20Uu3kArW8CKeH1AKXys8Y4YgNxGQ",
+  authDomain: "timesheet-14ba2.firebaseapp.com",
+  databaseURL: "https://timesheet-14ba2.firebaseio.com",
+  projectId: "timesheet-14ba2",
+  storageBucket: "",
+  messagingSenderId: "675252922865"
+};
+firebase.initializeApp(config);
