26a8895c7fc68bbe05f70c89dc57ab490801e93c
James Albert Phillips V <jamesalbertphillipsv@Jamess-MacBook-Air.local>
2018-08-14 13:31:16 -0700

    add on child function with new row and column scripts

--- main.js
+++ main.js
@@ -14,9 +14,30 @@ var employeeName;
 var role;
 var startDate;
 var monthlyRate;
+var monthsWorked;
+var totalBilled;
 
 // submit on click 
   // add new employee
 
 // on child added
   // populate table rows
+
+database.ref().on("childAdded", function(event) {
+  var newRow = $("<tr>")
+  // Columns in Current Employees Div
+  var tdName = $("<td>")
+  tdName.text(event.val().employeeName)
+  var tdRole = $("<td>")
+  tdRole.text(event.val().role)
+  var tdStart = $("<td>")
+  tdStart.text(event.val().startDate)
+  var tdWorked = $("<td>")
+  tdWorked.text(event.val().monthsWorked) 
+  var tdRate = $("<td>")
+  tdRate.text(event.val().monthlyRate)
+  var tdBilled = $("<td>")
+  tdBilled.text(event.val().totalBilled)
+  newRow.append(tdName, tdRole, tdStart, tdWorked, tdRate,  tdBilled)
+  $("tbody").append(newRow)
+})
